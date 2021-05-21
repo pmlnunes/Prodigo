14db7df196c751fd2b037848381a11718254f72f
pedromlopesnunes <pedromlopesnunes@gmail.com>
2021-05-22 00:14:21 +0100

    Added default heroku port.

--- prodigo.js
+++ prodigo.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 
 
-app.listen(5000, function() {
+app.listen(process.env.PORT || 5000, function() {
 	console.log(`Server is up and running on 5000 ...`);
   });
 	
