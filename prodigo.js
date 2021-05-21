47ea707d82c94bc4bf5712799642b4602613ce1b
pedromlopesnunes <pedromlopesnunes@gmail.com>
2021-05-22 00:07:38 +0100

    Added express.js

--- prodigo.js
+++ prodigo.js
@@ -1,6 +1,30 @@
 //import { getLyrics, getSong } from 'genius-lyrics-api';
 const genius = require("genius-lyrics-api")
 
+const express = require("express");
+  
+const app = express();
+
+
+app.listen(5000, function() {
+	console.log(`Server is up and running on 5000 ...`);
+  });
+	
+  app.get("/", (req, res) => {
+	
+	let data = {
+		name: "GFG",
+		age: 18,
+		male: true
+	}
+
+	genius.getLyrics(options).then((lyrics) => {
+		  res.send(chooseRandomLine(lyrics))
+	});
+	
+	  
+  });
+
 const options = {
 	apiKey: '4q-SJRXo7viuaFHlhUGJgrUJH8iAMfGemvMhhd0Tj9ElrpsnajIL-5xpDN5H6mOB',
 	title: 'Vamos Bafar',
@@ -8,7 +32,7 @@ const options = {
 	optimizeQuery: true
 };
 
-genius.getLyrics(options).then((lyrics) => chooseRandomLine(lyrics));
+
 
 /*genius.getSong(options).then((song) => {
 	chooseRandomLine(song.lyrics)
@@ -25,4 +49,6 @@ function chooseRandomLine(lyrics) {
 	const rndInt = Math.floor(Math.random() * arrayOfLines.length) + 1
 
 	console.log("Verso random do prodigo -> " + arrayOfLines[rndInt])
+
+	return arrayOfLines[rndInt]
 }
